8d19f2aab13ff16945ec9988816645b75cb8b4c7
Bobby Reay <rreay724@gmail.com>
2022-01-02 13:05:22 -0500

    styling

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,13 @@ import { useRouter } from "next/dist/client/router";
 import { useEffect, useState, useContext } from "react";
 import { UserContext, useUser } from "../context/userContext";
 import { Header, Sidebar } from "../components/index";
+import {
+  InboxIcon,
+  StarIcon,
+  ClockIcon,
+  PaperAirplaneIcon,
+  PlusIcon,
+} from "@heroicons/react/solid";
 
 export default function Home() {
   const { user } = useContext(UserContext);
@@ -26,10 +33,33 @@ export default function Home() {
         <title>Inbox - {user?.email}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
 
-      <div className="">
-        <Sidebar />
+      <div className="sticky top-0">
+        <Header />
+        <div className="bg-black-default w-64 h-screen  pl-5">
+          <div className="my-5 bg-white w-36 rounded-full h-12 flex items-center space-x-3 pl-2 cursor-pointer">
+            <PlusIcon className="w-8 text-yellow-600" />
+            <p className="text-sm">Compose</p>
+          </div>
+          <div className=" text-gray-300 space-y-3">
+            <div className="flex items-center space-x-3">
+              <InboxIcon className="w-6" />
+              <p className="text-sm">Inbox</p>
+            </div>
+            <div className="flex items-center space-x-3">
+              <StarIcon className="w-6" />
+              <p className="text-sm">Starred</p>
+            </div>
+            <div className="flex items-center space-x-3">
+              <ClockIcon className="w-6" />
+              <p className="text-sm">Snoozed</p>
+            </div>
+            <div className="flex items-center space-x-3">
+              <PaperAirplaneIcon className="w-6 rotate-90" />
+              <p className="text-sm">Sent</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
